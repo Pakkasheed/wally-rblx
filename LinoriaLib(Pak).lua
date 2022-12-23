ed6a97b63a48aaef6196072890c624da479de1ae
Pakkasheed <111211044+Pakkasheed@users.noreply.github.com>
2022-12-23 21:22:43 +0700

    Update LinoriaLib(Pak).lua

--- LinoriaLib(Pak).lua
+++ LinoriaLib(Pak).lua
@@ -29,9 +29,9 @@ local Library = {
     HudRegistry = {};
 
     FontColor = Color3.fromRGB(255, 255, 255);
-    MainColor = Color3.fromRGB(24, 24, 37);
-    BackgroundColor = Color3.fromRGB(22, 22, 31);
-    AccentColor = Color3.fromRGB(155, 184, 237);
+    MainColor = Color3.fromRGB(28, 28, 28);
+    BackgroundColor = Color3.fromRGB(20, 20, 20);
+    AccentColor = Color3.fromRGB(255, 130, 0);
     OutlineColor = Color3.fromRGB(50,50,50);
     
     Black = Color3.new(0, 0, 0);
